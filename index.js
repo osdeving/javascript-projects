e947abfe07215044373d2e16b4a8927a42c9f1cf
osdeving <osdeving@gmail.com>
2022-08-20 03:49:17 -0300

    leu arquivo

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
 import chalk from 'chalk'
+import fs from 'fs'
 
-console.log(chalk.blue('Hello world!'))
+function handleErr(err) {
+	throw new Error(chalk.red(err.code, 'não encontrou o arquivo'))
+}
+
+function readFile(path) {
+	const encoding = 'utf-8'
+	fs.readFile(path, encoding, (err, data) => {
+		if (err) {
+			handleErr(err)
+		}
+		console.log(chalk.green(data))
+	  })
+}
+
+readFile('./arquivos/teste.txt')
